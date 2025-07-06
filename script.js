8dafcae30571d8dfe0fb9d5a1acf08a0a1481e80
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-06 18:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let str = "";
+
+	while(str.length < l){
+		str += Math.random().toString(36).substring(2);
+	}
+
+	return str.substring(0,l);
 }
 
 // Do not change the code below.
